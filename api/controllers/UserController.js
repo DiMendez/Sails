c88cbb0488e9da666746e0cb8893708c165c7c6f
DiMendez <dianamendezavalos@gmail.com>
2017-06-23 08:57:59 -0500

    curso

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,7 +5,7 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-const series = require("async/series");
+const waterfall = require("async/waterfall");
 
 function addUser(err, req, cbwf){
 	if(!err){
@@ -16,34 +16,29 @@ function addUser(err, req, cbwf){
 		}).exec(cbwf);
 	}
 }
-function addUser2(req, cbwf){
-		return User.create({
-			nombre: req.body.nombre,
-			contraseña: req.body.contraseña,
-			edad: req.body.edad,
-		}).exec(cbwf);
-	}
-
 
 function createUser(req, res)
 {
-	series([
+	waterfall([
 		(cbwf)=>{ addUser(null, req, cbwf) },
-		(cbwf)=>{ addUser2(req, cbwf) },
 		], (err, user)=>{
 		if (err)
 	 		return res.status(500).send("Error")
 	 	else
-	 		return res.status(201).send("Creo al usuario");
+	 		return res.status(201).send("Creó al usuario");
 	});
-	
+
 }
 
 function readUsers(req, res)
 {
 	return User.find()
 		.then((foundUser)=>{
-			res.status(200).json(foundUser);
+			res.status(200).render("readUsers",{
+				title:"Usuarios",
+				users:foundUser,
+				layout:"layout",
+			});
 		}).catch((err)=>{
 			res.status(500).send("Algo ocurrió");
 		});
